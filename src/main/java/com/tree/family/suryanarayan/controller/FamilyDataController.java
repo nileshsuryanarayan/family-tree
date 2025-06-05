7c4523e6417289458ca54330ca57921edcbb2e0f
Rohit-Suryanarayan <79396407+Rohit-Suryanarayan@users.noreply.github.com>
2025-06-05 11:10:12 +0530

    New endpoint to update faamily member added

--- src/main/java/com/tree/family/suryanarayan/controller/FamilyDataController.java
+++ src/main/java/com/tree/family/suryanarayan/controller/FamilyDataController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tree.family.suryanarayan.model.FamilyMember;
@@ -24,6 +26,33 @@ public class FamilyDataController {
 	private ResponseEntity<List<FamilyMember>> getFamilyMembers() {
 		List<FamilyMember> list = null;
 		try {
+			list = service.getFamilyMembers();
+		} catch(IOException e) {
+			System.out.println("ERROR: " + e.getLocalizedMessage());
+			e.printStackTrace();
+		} catch(Exception e) {
+			System.out.println("ERROR: " + e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+		if(list == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		} else {
+			return ResponseEntity.ok(list);
+		}
+	}
+	
+	/** 
+	 * */
+	@PostMapping("/update-family-member")
+	private ResponseEntity<List<FamilyMember>> updateFamilyData(@RequestBody FamilyMember request) {
+		List<FamilyMember> list = null;
+		try {
+			// update Family Member
+			if(request == null && request.getId() <= 0) {
+				System.out.println("ERROR: INVALID REQUEST");
+				return new ResponseEntity(HttpStatus.BAD_REQUEST);
+			}
+			System.out.println("Update Family member request" + request);
 			list = service.getFamilyMembers();
 		} catch(IOException e) {
 			System.out.println("ERROR: " + e.getLocalizedMessage());
